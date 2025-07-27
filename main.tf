0fc8b5572007394b5428ff98d63f5bcce2d384e3
Vidyashree-30 <vidyasn30d@gmail.com>
2025-07-27 15:05:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,21 @@
-resource "null_resource" "create_directory" {
+resource "local_file" "file1" {
+  filename = "${path.module}/file1.txt"
+  content  = "This is file1"
+}
+
+resource "local_file" "file2" {
+  filename = "${path.module}/file2.txt"
+  content  = "This is file2"
+}
+
+resource "null_resource" "directory1" {
   provisioner "local-exec" {
-    command = "mkdir -p ${var.output_directory}"
+    command = "mkdir -p ${path.module}/dir1"
   }
 }
 
-resource "local_file" "example_file" {
-  content  = var.file_content
-  filename = "${var.output_directory}/${var.file_name}"
+resource "null_resource" "directory2" {
+  provisioner "local-exec" {
+    command = "mkdir -p ${path.module}/dir2"
+  }
 }
