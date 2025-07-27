e12b97b28c2387da29e9d2540b921465301fac26
Vidyashree-30 <vidyasn30d@gmail.com>
2025-07-27 16:24:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,32 @@
-# Create directories (cross-platform logic)
+variable "output_dirs" {
+  default = ["a", "b"]
+}
+
+variable "file_names" {
+  default = ["a/filea.txt", "b/fileb.txt"]
+}
+
+variable "file_content" {
+  default = "Hello from Terraform via Jenkins!"
+}
+
+# Create directories a and b
 resource "null_resource" "directories" {
   count = length(var.output_dirs)
 
   provisioner "local-exec" {
-    command = terraform.workspace == "windows" ? "mkdir ${var.output_dirs[count.index]}" : "mkdir -p ${var.output_dirs[count.index]}"
-    interpreter = terraform.workspace == "windows" ? ["cmd", "/C"] : ["/bin/bash", "-c"]
+    command = terraform.workspace == "windows" ?
+      "mkdir ${var.output_dirs[count.index]}" :
+      "mkdir -p ${var.output_dirs[count.index]}"
+      
+    interpreter = terraform.workspace == "windows" ?
+      ["cmd", "/C"] : ["/bin/bash", "-c"]
   }
 }
 
-# Create files inside the first directory
+# Create files filea.txt and fileb.txt
 resource "local_file" "files" {
   count    = length(var.file_names)
-  filename = "${path.module}/${var.output_dirs[0]}/${var.file_names[count.index]}"
+  filename = "${path.module}/${var.file_names[count.index]}"
   content  = var.file_content
-  file_permission = "0777"
-  directory_permission = "0777"
 }
