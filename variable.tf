95b141fb207686f921141ed63a8cfddb40c6b242
Vidyashree-30 <vidyasn30d@gmail.com>
2025-07-27 16:07:07 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,1 +1,15 @@
+variable "file_names" {
+  description = "List of files to create"
+  type        = list(string)
+}
+
+variable "output_dirs" {
+  description = "List of directories to create"
+  type        = list(string)
+}
+
+variable "file_content" {
+  description = "Content to write into the files"
+  type        = string
+}
 
