c62d9ce13cbdc1bfcd7618f739d087b926c9cbfd
Vidyashree-30 <vidyasn30d@gmail.com>
2025-07-27 16:25:31 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,17 +1,1 @@
-variable "file_names" {
-  description = "List of file names"
-  type        = list(string)
-  default     = ["file1.txt", "file2.txt"]
-}
 
-variable "file_content" {
-  description = "Content for files"
-  type        = string
-  default     = "Hello from Terraform via Jenkins!"
-}
-
-variable "output_dirs" {
-  description = "Directories to create"
-  type        = list(string)
-  default     = ["dir1", "dir2"]
-}
