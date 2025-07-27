f3205d1bfdd3915b2a4ced324ca3c0a52364d2dc
Vidyashree-30 <vidyasn30d@gmail.com>
2025-07-27 16:16:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,18 @@
-# Loop over each directory and create it using null_resource + local-exec
+# Create directories (cross-platform logic)
 resource "null_resource" "directories" {
   count = length(var.output_dirs)
 
   provisioner "local-exec" {
-    command = "${terraform.workspace == "windows" ? "mkdir ${var.output_dirs[count.index]}" : "mkdir -p ${var.output_dirs[count.index]}"}"
+    command = terraform.workspace == "windows" ? "mkdir ${var.output_dirs[count.index]}" : "mkdir -p ${var.output_dirs[count.index]}"
     interpreter = terraform.workspace == "windows" ? ["cmd", "/C"] : ["/bin/bash", "-c"]
   }
 }
 
-# Loop over each file and create it using local_file
+# Create files inside the first directory
 resource "local_file" "files" {
   count    = length(var.file_names)
-  filename = "${path.module}/${var.file_names[count.index]}"
+  filename = "${path.module}/${var.output_dirs[0]}/${var.file_names[count.index]}"
   content  = var.file_content
+  file_permission = "0777"
+  directory_permission = "0777"
 }
